139e3ea0e2abd38a0a3a4edbcad26153d7fc89d8
Julia Pitts <julia.pitts@outlook.com>
2017-07-02 16:08:26 -0700

    Expanded the character creation mock.

--- src/CharacterCreation/Types.fs
+++ src/CharacterCreation/Types.fs
@@ -3,4 +3,8 @@ module Wanderer.CharacterCreation.Types
 type InProgressCharacter = {
     Might : int
     Will : int
+    Persuasion : int
+    Combat : int
+    Ritual : int
+    Sneaking : int
 }
