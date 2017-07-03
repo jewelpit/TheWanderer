6f6d28e2ccd5206a2010fd63ba50fb7b4399e0bc
Julia Pitts <julia.pitts@outlook.com>
2017-07-02 21:23:25 -0700

    Basic character creation sort of almost works.

--- src/Wanderer.fs
+++ src/Wanderer.fs
@@ -6,20 +6,16 @@ open Elmish.React
 open Fable.Core.JsInterop
 
 open Wanderer.CharacterCreation
+open Wanderer.Model
 
 importAll "../css/main.css"
-
-type Model =
-    | CharacterCreation of InProgressCharacter
-
-type Message =
-    DoNothing
 
 let init () =
     CharacterCreation { Might = 3; Will = 3; Persuasion = 3; Combat = 3; Ritual = 3; Sneaking = 3 }
 
 let update (msg : Message) model =
-    model
+    match msg with
+    | UpdateCharacter ipc -> CharacterCreation ipc
 
 let view model dispatch =
     match model with
