af111632a061bc0e70834d193bc1c11b89044ae3
Julia Pitts <julia.pitts@outlook.com>
2017-07-03 01:01:15 -0700

    Character stats displayed on left in active game view.

--- src/ActiveGame.fs
+++ src/ActiveGame.fs
@@ -13,13 +13,45 @@ module R = Fable.Helpers.React
 module P = Fable.Helpers.React.Props
 
 let view (character, page) dispatch =
-    R.div [] [
-        R.p [] [R.str page.Text]
-        R.ul [] [
-            for cont in page.Continuations do
-                yield R.li [] [
-                    cont.Description
-                    R.br []
-                    R.button [P.OnClick (fun _ -> dispatch (Flip cont.NextPageName))] [R.str "Choose"]]
+    R.div [P.Style [P.Display "flex"]] [
+        R.table [] [
+            R.tbody [] [
+                R.tr [] [
+                    R.th [P.Style [P.TextAlign "right"]] [R.str "Might"]
+                    R.td [] [R.str <| string character.Might]
+                ]
+                R.tr [] [
+                    R.th [P.Style [P.TextAlign "right"]] [R.str "Will"]
+                    R.td [] [R.str <| string character.Will]
+                ]
+                R.tr [] [R.td [P.ColSpan 2.; P.Style [P.Height "1em"]] []]
+                R.tr [] [
+                    R.th [P.Style [P.TextAlign "right"]] [R.str "Persuasion"]
+                    R.td [] [R.str <| string character.Persuasion]
+                ]
+                R.tr [] [
+                    R.th [P.Style [P.TextAlign "right"]] [R.str "Combat"]
+                    R.td [] [R.str <| string character.Combat]
+                ]
+                R.tr [] [
+                    R.th [P.Style [P.TextAlign "right"]] [R.str "Ritual"]
+                    R.td [] [R.str <| string character.Ritual]
+                ]
+                R.tr [] [
+                    R.th [P.Style [P.TextAlign "right"]] [R.str "Sneaking"]
+                    R.td [] [R.str <| string character.Sneaking]
+                ]
+            ]
+        ]
+        R.div [P.ClassName "verticalDivider"] []
+        R.div [P.ClassName "storyArea"] [
+            R.p [] [R.str page.Text]
+            R.ul [] [
+                for cont in page.Continuations do
+                    yield R.li [] [
+                        cont.Description
+                        R.br []
+                        R.button [P.OnClick (fun _ -> dispatch (Flip cont.NextPageName))] [R.str "Choose"]]
+            ]
         ]
     ]
