9b93a805553e17b4de0b41bf9d0a3f2a0574878f
Julia Pitts <julia.pitts@outlook.com>
2017-07-02 23:05:34 -0700

    Players can start the game now.

--- src/Wanderer.fs
+++ src/Wanderer.fs
@@ -16,10 +16,21 @@ let init () =
 let update (msg : Message) model =
     match msg with
     | UpdateCharacter ipc -> CharacterCreation ipc
+    | StartGame ipc ->
+        {
+            Might = ipc.Might
+            Will = ipc.Will
+            Persuasion = if ipc.HighSkill = Persuasion then 4 else if ipc.LowSkill = Persuasion then 2 else 3
+            Combat = if ipc.HighSkill = Combat then 4 else if ipc.LowSkill = Combat then 2 else 3
+            Ritual = if ipc.HighSkill = Ritual then 4 else if ipc.LowSkill = Ritual then 2 else 3
+            Sneaking = if ipc.HighSkill = Sneaking then 4 else if ipc.LowSkill = Sneaking then 2 else 3
+        }
+        |> ActiveGame
 
 let view model dispatch =
     match model with
     | CharacterCreation character -> CharacterCreation.view character dispatch
+    | ActiveGame character -> Fable.Helpers.React.p [] [Fable.Helpers.React.str <| sprintf "%A" character]
 
 // App
 Program.mkSimple init update view
