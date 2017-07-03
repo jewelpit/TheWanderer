7238db9bdf2fcc28cd05f52cc3dfb3fdd4a39dcb
Julia Pitts <julia.pitts@outlook.com>
2017-07-02 23:31:13 -0700

    Active game pages now display.

--- src/ActiveGame.fs
+++ src/ActiveGame.fs
@@ -5,11 +5,12 @@ open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import.Browser
 
+open Wanderer.Data
 open Wanderer.Model
 open Wanderer.ViewHelpers
 
 module R = Fable.Helpers.React
 module P = Fable.Helpers.React.Props
 
-let view gameState dispatch =
-    R.p [] [R.str "Hey!"]
+let view (character, page) dispatch =
+    R.p [] [R.str page.Text]
