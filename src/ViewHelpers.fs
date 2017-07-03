8c84e061239ba77e6540ee22175333b754b2dd02
Julia Pitts <julia.pitts@outlook.com>
2017-07-02 23:20:27 -0700

    Moved some view helper methods around.

--- src/ViewHelpers.fs
+++ src/ViewHelpers.fs
@@ -1,8 +1,21 @@
 module Wanderer.ViewHelpers
 
+open Elmish
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable.Import.Browser
+
 module R = Fable.Helpers.React
 module P = Fable.Helpers.React.Props
 
 /// Wraps a string in a <p> tag.
 let para text =
     R.p [] [R.str text]
+
+/// Gets the value of the selected option in a select spinner.
+[<Emit("$0.options[$0.selectedIndex].value")>]
+let getSelectedValue (srcElement : Element) : string = jsNative
+
+/// Creates an option tag with the specified value and text.
+let makeValueOption value text selected =
+    R.option [P.Value <| U2.Case1 value; P.Selected selected] [R.str text]
