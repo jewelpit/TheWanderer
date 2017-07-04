1c12e22f2a6aa10af001021e987111ca610d10e5
Julia Pitts <julia@robo.church>
2017-07-03 20:07:48 -0700

    Base functionality for alternate page failures.

--- src/Data/Skills.fs
+++ src/Data/Skills.fs
@@ -13,10 +13,22 @@ type Attribute =
     | Will
 
 type RollResult = {
+    Succeeded : bool
     Target : int
     Rolls : int list
-    Attribute : Attribute
-    Skill : Skill
+    AttributeLevel : int
+    SkillRank : int
 }
 
-let private random = new Random()
+let private random = Random()
+
+let roll attribute skill target =
+    let rolls = List.init skill (fun _ -> random.Next(6) + 1)
+    printfn "%A" rolls
+    {
+        Succeeded = List.length (List.filter (fun r -> r <= attribute) rolls) >= target
+        Target = target
+        Rolls = rolls
+        AttributeLevel = attribute
+        SkillRank = skill
+    }
