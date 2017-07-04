e9e6fc248fc804f1b3fed7335f8bba12f13f94b1
Julia Pitts <julia@robo.church>
2017-07-03 22:43:40 -0700

    Injuries and stress display.

--- src/Views/ActiveGame.fs
+++ src/Views/ActiveGame.fs
@@ -13,37 +13,40 @@ open Wanderer.ViewHelpers
 module R = Fable.Helpers.React
 module P = Fable.Helpers.React.Props
 
+let private divider =
+    R.tr [] [R.td [P.ColSpan 2.; P.Style [P.Height "1em"]] []]
+
+let private makeStatRow title value =
+    R.tr [] [
+        R.th [P.Style [P.TextAlign "right"]] [R.str title]
+        R.td [] [R.str <| string value]
+    ]
+
+let private makeInjuryRow title value =
+    if value = 0 then
+        R.tr [] []
+    else
+        R.tr [P.ClassName "injured"] [
+            R.th [P.Style [P.TextAlign "right"]] [R.str title]
+            R.td [] [R.str <| string value]
+        ]
+
 let view (gameState : ActiveGameState) (result : Skills.RollResult option) dispatch =
     let character = gameState.Character
     let page = gameState.Page
     R.div [P.Style [P.Display "flex"]] [
         R.table [] [
             R.tbody [] [
-                R.tr [] [
-                    R.th [P.Style [P.TextAlign "right"]] [R.str "Might"]
-                    R.td [] [R.str <| string character.Might]
-                ]
-                R.tr [] [
-                    R.th [P.Style [P.TextAlign "right"]] [R.str "Will"]
-                    R.td [] [R.str <| string character.Will]
-                ]
-                R.tr [] [R.td [P.ColSpan 2.; P.Style [P.Height "1em"]] []]
-                R.tr [] [
-                    R.th [P.Style [P.TextAlign "right"]] [R.str "Persuasion"]
-                    R.td [] [R.str <| string character.Persuasion]
-                ]
-                R.tr [] [
-                    R.th [P.Style [P.TextAlign "right"]] [R.str "Combat"]
-                    R.td [] [R.str <| string character.Combat]
-                ]
-                R.tr [] [
-                    R.th [P.Style [P.TextAlign "right"]] [R.str "Ritual"]
-                    R.td [] [R.str <| string character.Ritual]
-                ]
-                R.tr [] [
-                    R.th [P.Style [P.TextAlign "right"]] [R.str "Sneaking"]
-                    R.td [] [R.str <| string character.Sneaking]
-                ]
+                makeStatRow "Might" character.Might
+                makeStatRow "Will" character.Will
+                divider
+                makeStatRow "Persuasion" character.Persuasion
+                makeStatRow "Combat" character.Combat
+                makeStatRow "Ritual" character.Ritual
+                makeStatRow "Sneaking" character.Sneaking
+                divider
+                makeInjuryRow "Injuries" character.Injuries
+                makeInjuryRow "Stress" character.Stress
             ]
         ]
         R.div [P.ClassName "verticalDivider"] []
